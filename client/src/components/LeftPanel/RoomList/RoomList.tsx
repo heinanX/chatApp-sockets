a3b2c2caf5d1d868eb8bd1576afa5ca4ab7e26a2
Sara Pellnor <114143422+SaraPellnor@users.noreply.github.com>
2023-08-18 21:12:56 +0200

    css roomlist

--- client/src/components/LeftPanel/RoomList/RoomList.tsx
+++ client/src/components/LeftPanel/RoomList/RoomList.tsx
@@ -1,12 +1,13 @@
 import { useSocket } from '../../../Context/SocketContext/SocketContext';
 //renderar ut alla rum i listan och sätter onClick på dom
 const RoomList = () => {
-  const { setCurrentRoom, roomsList } = useSocket();
+  const { currentRoom, setCurrentRoom, roomsList } = useSocket();
 
   return (
     <div className="roomList">
       {roomsList.map((roomName) => (
         <div 
+        className={currentRoom === roomName ? 'currentRoom' : ''}
         onClick={() => {setCurrentRoom(roomName)}}
         key={roomName}>
           {roomName}
