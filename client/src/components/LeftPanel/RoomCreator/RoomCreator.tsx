664707336e870fd4895df71cecc839d7add313e1
r-4u-1 <114141854+r-4u-1@users.noreply.github.com>
2023-08-30 23:56:44 +0200

    Feature: dialog is used

--- client/src/components/LeftPanel/RoomCreator/RoomCreator.tsx
+++ client/src/components/LeftPanel/RoomCreator/RoomCreator.tsx
@@ -1,37 +1,48 @@
 
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { useSocket } from "../../../Context/SocketContext/SocketContext"
 import "./roomCreator.css"
 // skapar rum och uppdaterar currentRoom och kör därmed useEffect i SocketContext
 const RoomCreator = () => {
-    const { setCurrentRoom } = useSocket()
-   const [value, setValue] = useState("")
-
-   // sätter currentRoom om inputfält inte är en tom ""
-   const createRoom = () => {
-    if ( value == "") alert("Rummet måste ha ett namn!")
+  const { setCurrentRoom } = useSocket()
+  const [value, setValue] = useState("")
+  const modalRef = useRef<HTMLDialogElement | null>(null);
+  // sätter currentRoom om inputfält inte är en tom ""
+  const createRoom = () => {
+    if (value == "") {
+      modalRef?.current?.showModal();
+      return;
+    }
     setCurrentRoom(value)
-  // Tömmer inputfältet
+    // Tömmer inputfältet
     setValue('')
   }
 
-   const enterKey = (e: React.KeyboardEvent<HTMLDivElement>) => {    
+  const enterKey = (e: React.KeyboardEvent<HTMLDivElement>) => {
     if (e.key == 'Enter') {
       createRoom()
     }
-
   }
+
+  const handleCloseDialog = () => {
+    modalRef?.current?.close();
+  };
+
   return (
-     <div className="roomCreator">
-    <input 
-      value={value}
-      onKeyDown={(e) => enterKey(e)}
-      onChange={(e) => setValue(e.target.value)}
-      placeholder= "skapa rum" type="text"
-    />
-    <button
-    onClick={createRoom}>+</button>
-</div>
+    <div className="roomCreator">
+      <dialog ref={modalRef}>
+        <div>Rummet måste ha ett namn!</div>
+        <button onClick={handleCloseDialog}>Stäng</button>
+      </dialog>
+      <input
+        value={value}
+        onKeyDown={(e) => enterKey(e)}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="skapa rum" type="text"
+      />
+      <button
+        onClick={createRoom}>+</button>
+    </div>
   )
 }
 
